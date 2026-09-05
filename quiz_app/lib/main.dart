204ecaa45397596dddc22ff376348bd4d8f81866
schwartz003a <kentvince.estopia@cit.edu>
2026-09-06 00:31:01 +0800

    Update Flutter quiz app

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -1,20 +1,51 @@
 import 'package:flutter/material.dart';
- 
+
 void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
         body: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(colors: [
-              const Color.fromARGB(255, 73, 7, 189),
-              const Color.fromARGB(255, 73, 6, 184),
-            ])
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 73, 7, 189),
+                Color.fromARGB(255, 73, 6, 184),
+              ],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
           ),
-        child: Center(
-          child: Text('Learn flutter the fun way')
- 
-        ),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/logo.png',
+                  width: 150,
+                ),
+
+                const SizedBox(height: 55),
+
+                const Text(
+                  'Learn Flutter the fun way!',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                ),
+
+                const SizedBox(height: 35),
+
+                const Text(
+                  '→   Start Quiz',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 14,
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     ),
