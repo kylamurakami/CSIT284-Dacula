fab872380237ede2b18fbad13d1218df6ebe395b
kylamurakami <kyledacula15@gmail.com>
2026-09-14 00:11:03 +0800

    Adjust questions summary layout

--- quiz_app/lib/questions_summary/questions_summary.dart
+++ quiz_app/lib/questions_summary/questions_summary.dart
@@ -10,7 +10,7 @@ class QuestionsSummary extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 400,
+      height: 380,
       child: SingleChildScrollView(
         child: Column(
           children: summaryData.map(
