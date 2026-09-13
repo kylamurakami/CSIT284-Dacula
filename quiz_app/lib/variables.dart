1719e2d98927cca03c3e753812b505798d20e1e1
kylamurakami <kyledacula15@gmail.com>
2026-09-13 14:36:48 +0800

    Update StyledText to accept text

--- quiz_app/lib/variables.dart
+++ quiz_app/lib/variables.dart
@@ -1,16 +1,1 @@
-import 'package:flutter/material.dart';
-
-class StyledText extends StatelessWidget {
-  const StyledText({super.key});
-
-  @override
-  Widget build(context) {
-    return const Text(
-      'Hello World!',
-      style: TextStyle(
-        color: Colors.white,
-        fontSize: 28,
-      ),
-    );
-  }
-}
+void main() {}
